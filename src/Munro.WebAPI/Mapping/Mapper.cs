9b2e667c9cb2494695d88a412bff46726d8c9121
Jens Rud Nielsen <jenrud@gmail.com>
2021-02-01 07:14:53 +0100

    Adds mapper for eventrequest -> eventjob. Could use AutoMapper for more complex objects.

---
+++ src/Munro.WebAPI/Mapping/Mapper.cs
@@ -0,0 +1,20 @@
+﻿using EventManager.WebAPI.Model;
+
+namespace EventManager.WebAPI.Mapping
+{
+    /// <summary>
+    /// Represents a simple mapper.
+    /// </summary>
+    public static class Mapper
+    {
+        public static EventJob Map(EventJobRequest source)
+        {
+            return new EventJob
+            {
+                Data = source.Data,
+                Name = source.Name,
+                UserName = source.UserName
+            };
+        }
+    }
+}
