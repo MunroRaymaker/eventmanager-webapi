ba199b9ed1c2a54d5b747e58cdf492ce02a3d9bc
Jens Rud Nielsen <jenrud@gmail.com>
2021-02-01 07:13:40 +0100

    Creates method to complete a job. Adds comments.

--- src/Munro.WebAPI/Model/EventJob.cs
+++ src/Munro.WebAPI/Model/EventJob.cs
@@ -2,22 +2,60 @@ using System;
 
 namespace EventManager.WebAPI.Model
 {
+    /// <summary>
+    /// Represents a job which holds data to be processed.
+    /// </summary>
     public class EventJob
     {
+        /// <summary>
+        /// Gets or sets the unique id. Uses integers for easy reference.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets a name for the job.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the user that initiated the job.
+        /// </summary>
         public string UserName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the data to be processed.
+        /// </summary>
         public int[] Data { get; set; }
 
+        /// <summary>
+        /// Gets or sets the timestamp when the job was created.
+        /// </summary>
         public DateTime TimeStamp { get; set; }
 
-        public long Duration { get; set; }
+        /// <summary>
+        /// Gets or sets the duration of the processing.
+        /// </summary>
+        public long Duration { get; private set; }
         
-        public bool IsCompleted { get; set; }
+        /// <summary>
+        /// Gets or sets if the job is completed.
+        /// </summary>
+        public bool IsCompleted { get; private set; }
 
-        public string Status { get; set; } // pending or completed
+        /// <summary>
+        /// Gets the status of the job.
+        /// </summary>
+        public string Status => IsCompleted ? "Completed" : "Pending";
+
+        public void Complete()
+        {
+            Duration = (DateTime.UtcNow - TimeStamp).Ticks;
+            IsCompleted = true;
+        }
+
+        public void ReProcess()
+        {
+            IsCompleted = false;
+        }
     }
 }
