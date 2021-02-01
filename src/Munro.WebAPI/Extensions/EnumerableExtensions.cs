72cb649fa23cee465621ba56d77b60ca0305ebb6
Jens Rud Nielsen <jenrud@gmail.com>
2021-02-01 07:17:20 +0100

    Refactoring and comments.

--- src/Munro.WebAPI/Extensions/EnumerableExtensions.cs
+++ src/Munro.WebAPI/Extensions/EnumerableExtensions.cs
@@ -6,6 +6,11 @@ namespace EventManager.WebAPI.Extensions
 {
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Shuffles an array of integers in random order.
+        /// </summary>
+        /// <param name="source">The input array.</param>
+        /// <returns>A shuffled array.</returns>
         public static IEnumerable<int> Shuffle(this IEnumerable<int> source)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
@@ -14,10 +19,10 @@ namespace EventManager.WebAPI.Extensions
 
             var array = source as int[] ?? source.ToArray();
 
-            for (int i = 0; i < array.Count(); i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 var tmp = array[i];
-                var newIndex = rnd.Next(0, array.Count() - 1);
+                var newIndex = rnd.Next(0, array.Length - 1);
                 array[i] = array[newIndex];
                 array[newIndex] = tmp;
             }
