9a5305ae26335be8bccaf27cda2833573dd6061f
Jens Rud Nielsen <jenrud@gmail.com>
2021-02-15 10:34:07 +0100

    Unit testing

---
+++ tests/Munro.UnitTests/ControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using EventManager.WebAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Munro.UnitTests
+{
+    public class ControllerTest : BaseTest
+    {
+        public DbContextOptions<JobContext> ContextOptions { get; }
+
+        public ControllerTest(DbContextOptions<JobContext> options)
+        {
+            ContextOptions = options;
+            Seed();
+        }
+
+        private void Seed()
+        {
+            using (var ctx = new JobContext(ContextOptions))
+            {
+                ctx.JobItems.AddRange(TestData);
+                ctx.SaveChanges();
+            }
+        }
+        
+        private static IEnumerable<Job> TestData
+        {
+            get
+            {
+                yield return new Job
+                {
+                    Name = "Johnny",
+                    UserName = "JRN",
+                    TimeStamp = DateTime.Now,
+                    Data = new[] { 3, 2, 1 }
+                };
+                yield return new Job
+                {
+                    Name = "Keira",
+                    UserName = "JRN",
+                    TimeStamp = DateTime.Now,
+                    Data = new[] { 5, 4, 3 }
+                };
+                yield return new Job
+                {
+                    Name = "Orlando",
+                    UserName = "JRN",
+                    TimeStamp = DateTime.Now,
+                    Data = new[] { 3000, 5000, 2000, 8000 }
+                };
+            }
+        }
+    }
+}
--- tests/Munro.UnitTests/JobsControllerTest.cs
+++ tests/Munro.UnitTests/JobsControllerTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EventManager.WebAPI.Controllers;
@@ -15,126 +14,90 @@ using Xunit;
 
 namespace Munro.UnitTests
 {
-    public class JobsControllerTest : BaseTest
+    public class JobsControllerTest : ControllerTest
     {
-        private static IEnumerable<Job> TestData
+        public JobsControllerTest()
+            : base(new DbContextOptionsBuilder<JobContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options)
         {
-            get
-            {
-                yield return new Job
-                {
-                    Id = 1, Name = "Johnny", UserName = "JRN", TimeStamp = DateTime.Now,
-                    Data = new[] {3, 2, 1}
-                };
-                yield return new Job
-                {
-                    Id = 2, Name = "Keira", UserName = "JRN", TimeStamp = DateTime.Now,
-                    Data = new[] {5, 4, 3}
-                };
-                yield return new Job
-                {
-                    Id = 3, Name = "Orlando", UserName = "JRN", TimeStamp = DateTime.Now,
-                    Data = new[] {3000, 5000, 2000, 8000}
-                };
-            }
         }
 
         [Fact]
         public async Task when_getting_all_jobs_should_return_jobs()
         {
             // Arrange
-            var options = new DbContextOptionsBuilder<JobContext>()
-                .UseInMemoryDatabase("JobsList")
-                .Options;
-
-            await using (var ctx = new JobContext(options))
-            {
-                await ctx.JobItems.AddRangeAsync(TestData);
-                await ctx.SaveChangesAsync();
-            }
-
             var logger = Mock.Of<ILogger<JobsController>>();
             var btq = Mock.Of<IBackgroundTaskQueue>();
             var worker = new Worker();
 
             // Use a clean context to run tests
-            await using var context = new JobContext(options);
-            var controller = new JobsController(logger, btq, this.Mapper, worker, context);
+            await using (var context = new JobContext(ContextOptions))
+            {
+                var controller = new JobsController(logger, btq, this.Mapper, worker, context);
 
-            // Act
-            var response = await controller.Get();
+                // Act
+                var response = await controller.Get();
 
-            // Assert
-            var jobs = response.Value.ToArray();
-            jobs.Should().BeOfType<Job[]>();
-            jobs.Length.Should().BeGreaterThan(0);
+                // Assert
+                var jobs = response.Value.ToArray();
+                jobs.Should().BeOfType<Job[]>();
+                jobs.Length.Should().BeGreaterThan(0);
+            }
         }
 
         [Fact]
         public async Task when_posting_returns_badrequest_when_model_is_invalid()
         {
             // Arrange
-            var options = new DbContextOptionsBuilder<JobContext>()
-                .UseInMemoryDatabase("JobsList")
-                .Options;
-
-            await using (var ctx = new JobContext(options))
-            {
-                await ctx.JobItems.AddRangeAsync(TestData);
-                await ctx.SaveChangesAsync();
-            }
-
             var logger = Mock.Of<ILogger<JobsController>>();
             var btq = Mock.Of<IBackgroundTaskQueue>();
             var worker = new Worker();
 
             // Use a clean context to run tests
-            await using var context = new JobContext(options);
-            var controller = new JobsController(logger, btq, this.Mapper, worker, context);
+            await using (var context = new JobContext(ContextOptions))
+            {
+                var controller = new JobsController(logger, btq, this.Mapper, worker, context);
 
-            controller.ModelState.AddModelError("Name", "Name cannot be empty");
+                controller.ModelState.AddModelError("Name", "Name cannot be empty");
 
-            // Act
-            var response = await controller.Create(new JobRequest());
+                // Act
+                var response = await controller.Create(new JobRequest());
 
-            // Assert
-            response.Result.Should().BeOfType<BadRequestObjectResult>();
+                // Assert
+                response.Result.Should().BeOfType<BadRequestObjectResult>();
+            }
         }
 
         [Fact]
         public async Task when_posting_returns_location_when_model_is_valid()
         {
             // Arrange
-            var options = new DbContextOptionsBuilder<JobContext>()
-                .UseInMemoryDatabase("JobsList")
-                .Options;
-
-            await using (var ctx = new JobContext(options))
-            {
-                await ctx.JobItems.AddRangeAsync(TestData);
-                await ctx.SaveChangesAsync();
-            }
-
             var logger = Mock.Of<ILogger<JobsController>>();
             var btq = Mock.Of<IBackgroundTaskQueue>();
             var worker = new Worker();
 
             // Use a clean context to run tests
-            await using var context = new JobContext(options);
-            var controller = new JobsController(logger, btq, this.Mapper, worker, context);
-
-            // Act
-            var response = await controller.Create(new JobRequest
+            await using (var transaction = new JobContext(ContextOptions).Database.CurrentTransaction)
+            await using (var context = new JobContext(ContextOptions))
             {
-                Data = new[] {3, 2, 1},
-                Name = "foo",
-                UserName = "bar"
-            });
+                var controller = new JobsController(logger, btq, this.Mapper, worker, context);
 
-            // Assert
-            response.Result.Should().BeOfType<CreatedAtActionResult>();
-            var result = response.Result as CreatedAtActionResult;
-            result.StatusCode.Should().Be(StatusCodes.Status201Created);
+                // Act
+                var response = await controller.Create(new JobRequest
+                {
+                    Data = new[] {3, 2, 1},
+                    Name = "foo",
+                    UserName = "bar"
+                });
+
+                // Assert
+                response.Result.Should().BeOfType<CreatedAtActionResult>();
+                var result = response.Result as CreatedAtActionResult;
+                result.StatusCode.Should().Be(StatusCodes.Status201Created);
+
+                await transaction.RollbackAsync();
+            }
         }
     }
 }
