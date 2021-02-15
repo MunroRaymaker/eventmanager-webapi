3332b32c25401aa28568e98a2945fc770b51f728
Jens Rud Nielsen <jenrud@gmail.com>
2021-02-15 08:34:40 +0100

    Refactored seed.

--- src/Munro.WebAPI/Program.cs
+++ src/Munro.WebAPI/Program.cs
@@ -4,7 +4,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
 using NLog.Web;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace EventManager.WebAPI
 {
@@ -18,13 +20,11 @@ namespace EventManager.WebAPI
             try
             {
                 logger.Debug("Initialized Main");
-                var host = CreateHostBuilder(args).Build();
-                
-                //Seed(host);
+                var host = CreateHostBuilder(args).Build().Seed();
 
                 host.Run();
             }
-            catch (System.Exception ex)
+            catch (Exception ex)
             {
                 // Catch setup errors
                 logger.Error(ex, "Stopped program because an exception occured");
@@ -33,7 +33,7 @@ namespace EventManager.WebAPI
             finally
             {
                 // Ensure to flush and stop internal timers/threads before application exit (avoid segmentation faults).
-                NLog.LogManager.Shutdown();
+                LogManager.Shutdown();
             }
         }
 
@@ -43,22 +43,25 @@ namespace EventManager.WebAPI
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>()
-                    .ConfigureLogging((hostingContext, logging) =>
-                    {
-                        logging.ClearProviders();
-                        logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                        logging.AddConsole();
-                        logging.SetMinimumLevel(LogLevel.Trace);
-                    })
-                    .UseNLog();
+                        .ConfigureLogging((hostingContext, logging) =>
+                        {
+                            logging.ClearProviders();
+                            logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                            logging.AddConsole();
+                            logging.SetMinimumLevel(LogLevel.Trace);
+                        })
+                        .UseNLog();
                 })
                 .ConfigureServices((hostContext, services) =>
                 {
                     // If you need to register the service early in the pipeline to allow for service startup, do it here.
                 });
         }
+    }
 
-        private static void Seed(IHost host)
+    public static class WebHostExtensions
+    {
+        public static IHost Seed(this IHost host)
         {
             using var scope = host.Services.CreateScope();
             var services = scope.ServiceProvider;
@@ -73,6 +76,8 @@ namespace EventManager.WebAPI
                 var logger = services.GetRequiredService<ILogger<Program>>();
                 logger.LogError(ex, "An error occurred creating the DB.");
             }
+
+            return host;
         }
     }
 }
